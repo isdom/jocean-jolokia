b60397af3c2c238776b2c01c332a08a7dbd85116
ming.ma <isdom.maming@gmail.com>
2018-03-27 17:37:34 +0800

    增加接口方法：list、readAttribute

--- src/main/java/org/jocean/jolokia/JolokiaAPI.java
+++ src/main/java/org/jocean/jolokia/JolokiaAPI.java
@@ -3,10 +3,16 @@ package org.jocean.jolokia;
 import org.jocean.http.Interact;
 import org.jocean.jolokia.spi.ExecResponse;
 import org.jocean.jolokia.spi.JolokiaRequest;
+import org.jocean.jolokia.spi.ListResponse;
+import org.jocean.jolokia.spi.ReadAttrResponse;
 
 import rx.Observable;
 import rx.functions.Func1;
 
 public interface JolokiaAPI {
-    public Func1<Interact, Observable<ExecResponse>> exec(final JolokiaRequest req);
+    public Func1<Interact, Observable<ListResponse>> list(final String uri);
+    
+    public Func1<Interact, Observable<ReadAttrResponse>> readAttribute(final String uri, final String mbean);
+    
+    public Func1<Interact, Observable<ExecResponse>> exec(final String uri, final JolokiaRequest req);
 }

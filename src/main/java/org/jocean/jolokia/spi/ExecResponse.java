5657c5fc20a0b667e20a0cdc2a8444ac1f61ecae
ming.ma <isdom.maming@gmail.com>
2018-04-18 13:13:18 +0800

    adjust code style

--- src/main/java/org/jocean/jolokia/spi/ExecResponse.java
+++ src/main/java/org/jocean/jolokia/spi/ExecResponse.java
@@ -3,7 +3,7 @@ package org.jocean.jolokia.spi;
 import com.alibaba.fastjson.annotation.JSONField;
 
 public class ExecResponse extends JolokiaResponse {
-    
+
     @JSONField(name="value")
     public Object getValue() {
         return _value;
@@ -13,7 +13,7 @@ public class ExecResponse extends JolokiaResponse {
     public void setValue(final Object value) {
         this._value = value;
     }
-    
+
     private Object _value;
 
     /* (non-Javadoc)
@@ -21,7 +21,7 @@ public class ExecResponse extends JolokiaResponse {
      */
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder();
+        final StringBuilder builder = new StringBuilder();
         builder.append("ExecValueResponse [value=").append(_value)
                 .append(", super=").append(super.toString()).append("]");
         return builder.toString();

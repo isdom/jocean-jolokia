92b3cfb7687badfb895b9a046ff7e6c9d3d3b74d
ming.ma <isdom@qq.com>
2021-01-13 19:03:45 +0800

    改造 jolokia api 为 RpcFacade 模式, 未完成

--- src/main/java/org/jocean/jolokia/JolokiaAPI.java
+++ src/main/java/org/jocean/jolokia/JolokiaAPI.java
@@ -1,15 +1,35 @@
 package org.jocean.jolokia;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
 import org.jocean.http.RpcRunner;
 import org.jocean.jolokia.api.ExecResponse;
 import org.jocean.jolokia.api.JolokiaRequest;
 import org.jocean.jolokia.api.JolokiaResponse;
 import org.jocean.jolokia.api.ListResponse;
 import org.jocean.jolokia.api.ReadAttrResponse;
+import org.jocean.rpc.annotation.RpcBuilder;
 
+import rx.Observable;
 import rx.Observable.Transformer;
 
 public interface JolokiaAPI {
+
+    @RpcBuilder
+    interface ListBuilder {
+
+        @Path("uri")
+        ListBuilder uri(final String uri);
+
+        @POST
+        @Path("${uri}")
+        @Consumes(MediaType.APPLICATION_JSON)
+        Observable<ListResponse> call();
+    }
+
     public Transformer<RpcRunner, ListResponse> list(final String uri);
 
     public Transformer<RpcRunner, ReadAttrResponse> readAttribute(final String uri, final String objectName);
